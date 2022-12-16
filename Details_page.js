c05a305a4404cb6f7b0729aa81d93d412bf1f87c
Yash Gokakkar <87797220+yashgokakkar99@users.noreply.github.com>
2022-12-16 10:58:01 +0530

    Create Details_page.js

---
+++ Details_page.js
@@ -0,0 +1,206 @@
+import React from 'react'
+
+import styles from './maskgroup.module.css'
+
+const Maskgroup = (props) => {
+  return (
+    <div>
+      <div className={styles['maskgroup']}>
+        <div className={styles['desktop2']}>
+          <div className={styles['desktop1']}>
+            <img
+              src="/playground_assets/rectangle6143-qt9r.svg"
+              alt="Rectangle6143"
+              className={styles['rectangle6']}
+            />
+            <img
+              src="/playground_assets/rectangle7144-86f-500w.png"
+              alt="Rectangle7144"
+              className={styles['rectangle7']}
+            />
+            <img
+              src="/playground_assets/rectangle232114-w5al-200h.png"
+              alt="Rectangle232114"
+              className={styles['rectangle23']}
+            />
+            <img
+              src="/playground_assets/rectangle8145-q3f-500w.png"
+              alt="Rectangle8145"
+              className={styles['rectangle8']}
+            />
+            <img
+              src="/playground_assets/rectangle252117-fnqmi-200h.png"
+              alt="Rectangle252117"
+              className={styles['rectangle25']}
+            />
+            <img
+              src="/playground_assets/rectangle9146-cwe9-500w.png"
+              alt="Rectangle9146"
+              className={styles['rectangle9']}
+            />
+            <img
+              src="/playground_assets/rectangle10147-mp5i-500w.png"
+              alt="Rectangle10147"
+              className={styles['rectangle10']}
+            />
+            <img
+              src="/playground_assets/rectangle11148-35gd-500w.png"
+              alt="Rectangle11148"
+              className={styles['rectangle11']}
+            />
+            <img
+              src="/playground_assets/vector126-bf3.svg"
+              alt="Vector126"
+              className={styles['vector']}
+            />
+            <img
+              src="/playground_assets/rectangle17192-ks77-200h.png"
+              alt="Rectangle17192"
+              className={styles['rectangle17']}
+            />
+            <img
+              src="/playground_assets/rectangle2193-8bm7.svg"
+              alt="Rectangle2193"
+              className={styles['rectangle2']}
+            />
+            <img
+              src="/playground_assets/rectangle18218-nyv5-200h.png"
+              alt="Rectangle18218"
+              className={styles['rectangle18']}
+            />
+            <img
+              src="/playground_assets/rectangle19219-65-200h.png"
+              alt="Rectangle19219"
+              className={styles['rectangle19']}
+            />
+            <img
+              src="/playground_assets/rectangle202110-qeok-200h.png"
+              alt="Rectangle202110"
+              className={styles['rectangle20']}
+            />
+            <img
+              src="/playground_assets/rectangle212111-s9c6-200h.png"
+              alt="Rectangle212111"
+              className={styles['rectangle21']}
+            />
+            <img
+              src="/playground_assets/rectangle222112-y8mf-200h.png"
+              alt="Rectangle222112"
+              className={styles['rectangle22']}
+            />
+            <span className={styles['text']}>
+              <span>Websites</span>
+            </span>
+            <img
+              src="/playground_assets/rectangle242115-ywf-200h.png"
+              alt="Rectangle242115"
+              className={styles['rectangle24']}
+            />
+            <span className={styles['text02']}>
+              <span>Youtube Channels</span>
+            </span>
+            <img
+              src="/playground_assets/rectangle262118-mms7-200h.png"
+              alt="Rectangle262118"
+              className={styles['rectangle26']}
+            />
+            <span className={styles['text04']}>
+              <span>Project topics</span>
+            </span>
+            <img
+              src="/playground_assets/rectangle272120-167j-200h.png"
+              alt="Rectangle272120"
+              className={styles['rectangle27']}
+            />
+            <span className={styles['text06']}>
+              <span>Road Map</span>
+            </span>
+            <img
+              src="/playground_assets/rectangle282122-i3jn-200h.png"
+              alt="Rectangle282122"
+              className={styles['rectangle28']}
+            />
+            <span className={styles['text08']}>
+              <span>Online courses</span>
+            </span>
+            <img
+              src="/playground_assets/rectangle292124-4az6-200h.png"
+              alt="Rectangle292124"
+              className={styles['rectangle29']}
+            />
+            <span className={styles['text10']}>
+              <span>Suggestions</span>
+            </span>
+            <img
+              src="/playground_assets/rectangle302126-rlcl-300h.png"
+              alt="Rectangle302126"
+              className={styles['rectangle30']}
+            />
+            <img
+              src="/playground_assets/rectangle312127-muho-300h.png"
+              alt="Rectangle312127"
+              className={styles['rectangle31']}
+            />
+            <img
+              src="/playground_assets/rectangle322129-i63-200w.png"
+              alt="Rectangle322129"
+              className={styles['rectangle32']}
+            />
+            <img
+              src="/playground_assets/rectangle332130-xh7p-200w.png"
+              alt="Rectangle332130"
+              className={styles['rectangle33']}
+            />
+            <img
+              src="/playground_assets/rectangle342131-b1nw-200w.png"
+              alt="Rectangle342131"
+              className={styles['rectangle34']}
+            />
+            <img
+              src="/playground_assets/rectangle352132-ktq-200w.png"
+              alt="Rectangle352132"
+              className={styles['rectangle35']}
+            />
+            <img
+              src="/playground_assets/rectangle362135-jfoo-200w.png"
+              alt="Rectangle362135"
+              className={styles['rectangle36']}
+            />
+            <img
+              src="/playground_assets/rectangle372136-o9ce-300h.png"
+              alt="Rectangle372136"
+              className={styles['rectangle37']}
+            />
+            <img
+              src="/playground_assets/ellipse12137-cwcf-300h.png"
+              alt="Ellipse12137"
+              className={styles['ellipse1']}
+            />
+            <span className={styles['text12']}>
+              <span>
+                <span>Click here</span>
+                <br></br>
+                <span>
+                  {' '}
+                  for
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+                <br></br>
+                <span>suggesting</span>
+              </span>
+            </span>
+          </div>
+          <span className={styles['text19']}>
+            <span>All rights reserved</span>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Maskgroup
