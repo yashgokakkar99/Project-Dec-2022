818994a8e08c74a8cb4bd8446433b3831b34f59b
Ashutosh Raj Gupta <89141709+AshutoshRajGupta@users.noreply.github.com>
2022-12-16 12:40:00 +0530

    Add files via upload

---
+++ reactpart/Aboutus/About.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+import styles from './desktop4.module.css'
+
+const Desktop4 = (props) => {
+  return (
+    <div>
+      <div className={styles['desktop4']}>
+        <img
+          src="/playground_assets/rectangle17222-cs6j-200h.png"
+          alt="Rectangle17222"
+          className={styles['rectangle17']}
+        />
+        <img
+          src="/playground_assets/vector225-2srf.svg"
+          alt="Vector225"
+          className={styles['vector']}
+        />
+        <img
+          src="/playground_assets/rectangle392214-sqyl-200h.png"
+          alt="Rectangle392214"
+          className={styles['rectangle39']}
+        />
+        <span className={styles['text']}>
+          <span>About us</span>
+        </span>
+        <img
+          src="/playground_assets/ellipse2322-iim-300w.png"
+          alt="Ellipse2322"
+          className={styles['ellipse2']}
+        />
+        <img
+          src="/playground_assets/ellipse3323-u6kv-300w.png"
+          alt="Ellipse3323"
+          className={styles['ellipse3']}
+        />
+        <img
+          src="/playground_assets/rectangle40324-mu8i-200h.png"
+          alt="Rectangle40324"
+          className={styles['rectangle40']}
+        />
+        <span className={styles['text02']}>
+          <span>
+            WEB DEV
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+        </span>
+        <img
+          src="/playground_assets/rectangle41325-3k7-200h.png"
+          alt="Rectangle41325"
+          className={styles['rectangle41']}
+        />
+        <img
+          src="/playground_assets/rectangle42326-ywvh-200h.png"
+          alt="Rectangle42326"
+          className={styles['rectangle42']}
+        />
+        <span className={styles['text04']}>
+          <span>VISION</span>
+        </span>
+        <span className={styles['text06']}>
+          <span>
+            WEB DEV
+            <span
+              dangerouslySetInnerHTML={{
+                __html: ' ',
+              }}
+            />
+          </span>
+        </span>
+        <img
+          src="/playground_assets/rectangle23332-zkca.svg"
+          alt="Rectangle23332"
+          className={styles['rectangle2']}
+        />
+        <span className={styles['text08']}>
+          <span>All rights reserved</span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Desktop4
---
+++ reactpart/contactus/contact.js
@@ -0,0 +1,128 @@
+import React from 'react'
+
+import styles from './desktop3.module.css'
+
+const Desktop3 = (props) => {
+  return (
+    <div>
+      <div className={styles['desktop3']}>
+        <img
+          src="/playground_assets/rectangle1272-r4b-200h.png"
+          alt="Rectangle1272"
+          className={styles['rectangle12']}
+        />
+        <span className={styles['text']}>
+          <span>Contact Us</span>
+        </span>
+        <img
+          src="/playground_assets/rectangle17223-tfql-200h.png"
+          alt="Rectangle17223"
+          className={styles['rectangle17']}
+        />
+        <img
+          src="/playground_assets/vector224-267q.svg"
+          alt="Vector224"
+          className={styles['vector']}
+        />
+        <img
+          src="/playground_assets/rectangle23330-2ykn.svg"
+          alt="Rectangle23330"
+          className={styles['rectangle2']}
+        />
+        <span className={styles['text02']}>
+          <span>All rights reserved</span>
+        </span>
+        <img
+          src="/playground_assets/rectangle52454-ibgi-200h.png"
+          alt="Rectangle52454"
+          className={styles['rectangle52']}
+        />
+        <span className={styles['text04']}>
+          <span>SUBMIT</span>
+        </span>
+        <img
+          src="/playground_assets/rectangle53456-ei1-200h.png"
+          alt="Rectangle53456"
+          className={styles['rectangle53']}
+        />
+        <img
+          src="/playground_assets/rectangle54458-x9of-200h.png"
+          alt="Rectangle54458"
+          className={styles['rectangle54']}
+        />
+        <img
+          src="/playground_assets/rectangle55459-81s-200h.png"
+          alt="Rectangle55459"
+          className={styles['rectangle55']}
+        />
+        <span className={styles['text06']}>
+          <span>Name</span>
+        </span>
+        <span className={styles['text08']}>
+          <span>Email</span>
+        </span>
+        <span className={styles['text10']}>
+          <span>Phone no</span>
+        </span>
+        <img
+          src="/playground_assets/rectangle57462-lhe-200h.png"
+          alt="Rectangle57462"
+          className={styles['rectangle57']}
+        />
+        <img
+          src="/playground_assets/rectangle58463-392l-200h.png"
+          alt="Rectangle58463"
+          className={styles['rectangle58']}
+        />
+        <img
+          src="/playground_assets/rectangle59464-exgi-200h.png"
+          alt="Rectangle59464"
+          className={styles['rectangle59']}
+        />
+        <span className={styles['text12']}>
+          <span>WhatsApp</span>
+        </span>
+        <span className={styles['text14']}>
+          <span>9977665588</span>
+        </span>
+        <span className={styles['text16']}>
+          <span>EMAIL</span>
+        </span>
+        <span className={styles['text18']}>
+          <span>xyz@gmail.com</span>
+        </span>
+        <span className={styles['text20']}>
+          <span>Facebook</span>
+        </span>
+        <span className={styles['text22']}>
+          <span>facebook id</span>
+        </span>
+        <img
+          src="/playground_assets/image34613-p8w-200h.png"
+          alt="image34613"
+          className={styles['image3']}
+        />
+        <img
+          src="/playground_assets/image44615-fh5o-200h.png"
+          alt="image44615"
+          className={styles['image4']}
+        />
+        <img
+          src="/playground_assets/image54617-n8p-200h.png"
+          alt="image54617"
+          className={styles['image5']}
+        />
+        <img
+          src="/playground_assets/rectangle604618-dwdl-200h.png"
+          alt="Rectangle604618"
+          className={styles['rectangle60']}
+        />
+        <span className={styles['text24']}>
+          <span>Message</span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Desktop3
