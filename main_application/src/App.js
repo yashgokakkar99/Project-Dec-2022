10168f3210dcb0528c9db2fde37bea83627578e8
Ashutosh Raj Gupta <ag2364443@gmail.com>
2022-12-18 21:08:26 +0530

    contact us page finished

--- main_application/src/App.js
+++ main_application/src/App.js
@@ -5,11 +5,13 @@ import Main from './Components/main/Main';
 import Contact from './Components/contactus/Contact';
 import Footer from './Components/Footer/Footer';
 import Suggestion from './Components/Suggestion/Suggestion'
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      {/* <h1>Hello world</h1> */}
+      <Navbar/>
       <Home/>
       <Main/>
       <About/>
