c8756cabc80d65cd2aeb9fdef8c24447fc208d35
Yash Gokakkar <yashgokakkar@gmail.com>
2022-12-19 13:14:43 +0530

    Main page skeleton ready

--- main_application/src/Components/main/Main.jsx
+++ main_application/src/Components/main/Main.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import './Main.css'
 
 const Main = () => {
   return (
     <section id='Main'>
-        <h3>Main page</h3>
+        <div className="maindiv_content">
+          <div className="offi_websites">
+            <h4>Official websites</h4>
+          </div>
+          <div className="yt_videos">
+            <h4>Youtube videos</h4>
+          </div>
+          <div className="Projects">
+            <h4>Projects</h4>
+          </div>
+          <div className="Roadmap">
+            <h4>Roadmap</h4>
+          </div>
+          <div className="Courses">
+            <h4>Courses</h4>
+          </div>
+          <div className="Suggestion_form">
+            <h4>Suggestions form</h4>
+          </div>
+        </div>
     </section>
   )
 }
