ad3690f2b97df2144f836ed2100213507deb2a6a
Yash Gokakkar <yashgokakkar@gmail.com>
2022-12-19 08:21:55 +0530

    Home page created

--- main_application/src/Components/Home/Home.jsx
+++ main_application/src/Components/Home/Home.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
     <section id='Home'>
-        <h3>Home</h3>
+        <div className="maindiv">
+          <div className="rect1">
+            <h4>Front-end</h4>
+          </div>
+          <div className="rect2">
+            <h4>Backend</h4>
+          </div>
+          <div className="rect3">
+            <h4>DSA</h4>
+          </div>
+          <div className="rect4">
+            <h4>Android Dev</h4>
+          </div>
+          <div className="rect5">
+            <h4>Cloud computing</h4>
+          </div>
+          <div className="rect6">
+            <h4>AI</h4>
+          </div>
+          <div className="rect7">
+            <h4>ML</h4>
+          </div>
+          <div className="rect8">
+            <h4>Cyber security</h4>
+          </div>
+          <div className="rect9">
+            <h4>Graphics design</h4>
+          </div>
+        </div>
     </section>
   )
 }
