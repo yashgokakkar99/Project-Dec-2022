fb26a79ba1aa45ab82000e12fb5991011224904c
Yash Gokakkar <yashgokakkar@gmail.com>
2022-12-19 20:11:34 +0530

    navbar updated

--- main_application/src/Components/Navbar/Navbar.js
+++ main_application/src/Components/Navbar/Navbar.js
@@ -4,7 +4,19 @@ import './Navbar_style.css'
 const Navbar = () => {
   return (
     <section id="Navbar">
-      
+      <div className="nav">
+        <ul className='nav_con'>
+          <li className='nav_items' id='nav1'>
+            <a href="#">Home</a>
+          </li>
+          <li className='nav_items' id='nav2'>
+            <a href="#About">About us</a>
+          </li>
+          <li className='nav_items' id='nav3'>
+            <a href="#contact">Contact us</a>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
