ecc629bd1904aaf1c2238848144480936eac1445
Yash Gokakkar <yashgokakkar@gmail.com>
2022-12-19 19:13:20 +0530

    home page with images updated

--- main_application/src/Components/Home/Home.jsx
+++ main_application/src/Components/Home/Home.jsx
@@ -1,40 +1,76 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
+import img1 from "../../assets/Images/frontend.png";
+import img2 from "../../assets/Images/backend.png";
+import img3 from "../../assets/Images/dsa.png";
+import img4 from "../../assets/Images/android.png";
+import img5 from "../../assets/Images/cloud.png";
+import img6 from "../../assets/Images/cyber.png";
+import img7 from "../../assets/Images/ai.png";
+import img8 from "../../assets/Images/ml.png";
+import img9 from "../../assets/Images/graphics.png";
 
 const Home = () => {
   return (
-    <section id='Home'>
-        <div className="maindiv">
-          <div className="rect1">
-            <h4>Front-end</h4>
-          </div>
-          <div className="rect2">
-            <h4>Backend</h4>
-          </div>
-          <div className="rect3">
-            <h4>DSA</h4>
-          </div>
-          <div className="rect4">
-            <h4>Android Dev</h4>
-          </div>
-          <div className="rect5">
-            <h4>Cloud computing</h4>
-          </div>
-          <div className="rect6">
-            <h4>AI</h4>
-          </div>
-          <div className="rect7">
-            <h4>ML</h4>
-          </div>
-          <div className="rect8">
-            <h4>Cyber security</h4>
-          </div>
-          <div className="rect9">
-            <h4>Graphics design</h4>
+    <section id="Home">
+      <div className="maindiv">
+        <div className="rect1">
+          <h4>Front-end</h4>
+          <div className="i1">
+            <img className="image1" src={img1}></img>
           </div>
         </div>
+        <div className="rect2">
+          <h4>Backend</h4>
+          <div className="i1">
+            <img className="image2" src={img2}></img>
+          </div>
+        </div>
+        <div className="rect3">
+          <h4>DSA</h4>
+          <div className="i1">
+            <img className="image3" src={img3}></img>
+          </div>
+        </div>
+        <div className="rect4">
+          <h4>Android Dev</h4>
+          <div className="i1">
+            <img className="image4" src={img4}></img>
+          </div>
+        </div>
+        <div className="rect5">
+          <h4>Cloud computing</h4>
+          <div className="i1">
+            <img className="image5" src={img5}></img>
+          </div>
+        </div>
+        <div className="rect6">
+          <h4>AI</h4>
+          <div className="i1">
+            <img className="image6" src={img6}></img>
+          </div>
+        </div>
+        <div className="rect7">
+          <h4>ML</h4>
+          <div className="i1">
+            <img className="image7" src={img7}></img>
+          </div>
+        </div>
+        <div className="rect8">
+          <h4>Cyber security</h4>
+          <div className="i1">
+            <img className="image8" src={img8}></img>
+          </div>
+        </div>
+        <div className="rect9">
+          <h4>Graphics design</h4>
+          <div className="i1">
+            <img className="image9" src={img9}></img>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- main_application/src/Components/main/Main.jsx
+++ main_application/src/Components/main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Main.css'
 
+
 const Main = () => {
   return (
     <section id='Main'>
