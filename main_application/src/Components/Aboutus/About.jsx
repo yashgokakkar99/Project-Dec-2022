c71618db89a07da3d778dad822004400770c364e
Yash Gokakkar <yashgokakkar@gmail.com>
2022-12-20 19:35:03 +0530

    main page updated

--- main_application/src/Components/Aboutus/About.jsx
+++ main_application/src/Components/Aboutus/About.jsx
@@ -1,58 +1,39 @@
-import React from 'react'
-import './About.css'
-import IMG1 from '../../assets/yash_profile.jpg'
-import IMG2 from '../../assets/Ashutosh_profile.jpg'
-
+import React from "react";
+import "./About.css";
+import IMG1 from "../../assets/yash_profile.jpg";
+import IMG2 from "../../assets/Ashutosh_profile.jpg";
 
 const About = () => {
   return (
-    <section id='About'>
-        <h2>About us</h2>
-        <div className="about__text">
-          <h3>VISION</h3>
+    <section id="About">
+      <h2>About us</h2>
+      <div className="about__text">
+        <h3>VISION</h3>
 
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-          Officiis repellat harum similique cumque, recusandae aut maxime
-           facilis enim ex ea saepe neque quis quasi aliquid rem atque 
-           praesentium optio illum?
-           </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis
+          repellat harum similique cumque, recusandae aut maxime facilis enim ex
+          ea saepe neque quis quasi aliquid rem atque praesentium optio illum?
+        </p>
 
-           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis mollitia saepe <br></br>
-            consequuntur nam itaque ex sequi omnis accusamus iure! Fugiat inventore nisi, <br></br>
-            cum sint adipisci suscipit officiis perferendis debitis id!
-           </p>
-          
-        </div>
-        <div
-        className="container portfolio__container"
-        modules={[Pagination]}
-        spaceBetween={40}
-        slidesPerView={2}
-        navigation
-        pagination={{ clickable: true }}
-      
-      >
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+          mollitia saepe <br></br>
+          consequuntur nam itaque ex sequi omnis accusamus iure! Fugiat
+          inventore nisi, <br></br>
+          cum sint adipisci suscipit officiis perferendis debitis id!
+        </p>
+      </div>
+      <div className="portfolio__container">
+          <div className="portfolio_img">
             <img src={IMG1} alt="" />
           </div>
-          
-          
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
+          <div className="portfolio_img">
             <img src={IMG2} alt="" />
-          </div>
-          
-          
-        </article>
-        
-        
+        </div>
       </div>
-        
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
