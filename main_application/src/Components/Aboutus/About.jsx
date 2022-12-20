0f62da20778fffad8ee89588292a1187bd0431ab
Ashutosh Raj Gupta <ag2364443@gmail.com>
2022-12-20 16:00:56 +0530

    updated

--- main_application/src/Components/Aboutus/About.jsx
+++ main_application/src/Components/Aboutus/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './About.css'
+import IMG1 from '../../assets/yash_profile.jpg'
+import IMG2 from '../../assets/Ashutosh_profile.jpg'
+
 
 const About = () => {
   return (
@@ -15,10 +18,38 @@ const About = () => {
            </p>
 
            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis mollitia saepe <br></br>consequuntur nam itaque ex sequi omnis accusamus iure! Fugiat inventore nisi, <br></br>cum sint adipisci suscipit officiis perferendis debitis id!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis mollitia saepe <br></br>
+            consequuntur nam itaque ex sequi omnis accusamus iure! Fugiat inventore nisi, <br></br>
+            cum sint adipisci suscipit officiis perferendis debitis id!
            </p>
           
         </div>
+        <div
+        className="container portfolio__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={2}
+        navigation
+        pagination={{ clickable: true }}
+      
+      >
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          
+          
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          
+          
+        </article>
+        
+        
+      </div>
         
     </section>
   )
