1a49b98f5264a711ef508b64af9a1a83199543c3
Ashutosh Raj Gupta <ag2364443@gmail.com>
2022-12-22 02:35:44 +0530

    navbar dark mode created

--- main_application/src/Components/Navbar/Navbar.js
+++ main_application/src/Components/Navbar/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Navbar.css'
-import { useState } from "react";
+// import { useState } from "react";
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+
+
+
+export const ThemeContext = createContext(null);
 
 const Navbar = () => {
+  
   const [activeNav, setActiveNav] = useState("#");
+
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
+
   return (
     <section id="Navbar">
 
@@ -30,6 +45,17 @@ const Navbar = () => {
         Contact-us
       </a>
     </nav>
+
+    
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        {/* <Form /> */}
+        <div className="switch">
+          <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        </div>
+      </div>
+    </ThemeContext.Provider>
 
     </section>
   );
