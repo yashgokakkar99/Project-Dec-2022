b8ed183e9e7c6e9a9d435d800c478ad0ced39589
Ashutosh Raj Gupta <ag2364443@gmail.com>
2022-12-24 19:13:56 +0530

    updated

--- main_application/src/App.js
+++ main_application/src/App.js
@@ -1,29 +1,51 @@
-import './App.css';
-import Home from './Components/Home/Home';
-import About from './Components/Aboutus/About';
-import Main from './Components/main/Main';
-import Contact from './Components/contactus/Contact';
-import Footer from './Components/Footer/Footer';
-import Suggestion from './Components/Suggestion/Suggestion'
-import Navbar from './Components/Navbar/Navbar';
-import {useState, useEffect} from 'react';
+import "./App.css";
+import Home from "./Components/Home/Home";
+import About from "./Components/Aboutus/About";
+import Main from "./Components/main/Main";
+import Contact from "./Components/contactus/Contact";
+import Footer from "./Components/Footer/Footer";
+import Suggestion from "./Components/Suggestion/Suggestion";
+import Navbar from "./Components/Navbar/Navbar";
+import { useState, useEffect } from "react";
+import HashLoader from "react-spinners/HashLoader";
 
-
+function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+  const style = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  };
 
   return (
     <div className="App">
-      {
-        
-     <>
-      <Navbar/>
-      <Home/>
-      <About/>
-      <Suggestion/>
-      <Contact/>
-      <Footer/>
-     </>
-      }
-      
+      {loading ? (
+        <HashLoader
+          color={"#D0021B"}
+          loading={loading}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          style={style}
+          
+        />
+      ) : (
+        <>
+          <Navbar />
+          <Home />
+          <About />
+          <Suggestion />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
