fb3ecccddccc1a3f6aaf5b2f9b33aee99ac19d35
Yash Gokakkar <yashgokakkar@gmail.com>
2022-12-29 18:44:59 +0530

    About us updated

--- main_application/src/Components/Aboutus/About.jsx
+++ main_application/src/Components/Aboutus/About.jsx
@@ -2,49 +2,49 @@ import React from "react";
 import "./About.css";
 import IMG1 from "../../assets/yash_profile.jpg";
 import IMG2 from "../../assets/Ashutosh_profile.jpg";
-import {FaRegHandshake} from "react-icons/fa";
-import {FiDownload} from "react-icons/fi";
+import { FaRegHandshake } from "react-icons/fa";
+import { FiDownload } from "react-icons/fi";
 import cv1 from "../../assets/cv_Yash.pdf";
 import cv2 from "../../assets/cv_Ashutosh.pdf";
 import Lottie from "lottie-react";
 import hand1 from "../../assets/lotties/handshake.json";
 
-
 const About = () => {
   return (
     <section id="About">
-      
       <div className="about__text">
         <h2>About Us</h2>
-        <p>
-          map2proCoder is developed to provide essential links to study material available online. So that any student start working towards
-          his/her career in Computer Science in various domains such as web dev, android dev, cloud computing, cyber security and many more.
-        </p>
+        <div className="con">
+          "map2proCoder" is developed to provide essential links to study material
+          available online. So that any student start working towards his/her
+          career in Computer Science in various domains such as web dev, android
+          dev, cloud computing, cyber security and many more.
+        </div>
       </div>
       <div className="portfolio__container">
-          <div className="portfolio_img">
-            <img src={IMG1} alt="" />
-            <h3>YASH GOKKAKAR</h3>
-            <a href={cv1} target="_blank" download className="btn">
+        <div className="portfolio_img">
+          <img src={IMG1} alt="" />
+          <h3>YASH GOKKAKAR</h3>
+          <a href={cv1} target="_blank" download className="btn">
             <FiDownload size={30} />
-            </a>
-            <a href="https://linktr.ee/yash_gokakkar" target="_blank" className="btn">
-            <FaRegHandshake size={30}/>
-            </a>
-            
-          </div>
-          <div className="portfolio_img">
-            <img src={IMG2} alt="" />
-            <h3> ASHUTOSH RAJ GUPTA </h3>
-            <a href={cv2} target="_blank" download className="btn">
-            <FiDownload size={30} />
-          
-            </a>
-            <a href="https://linktr.ee/ashu_2643" target="_blank" className="btn">
+          </a>
+          <a
+            href="https://linktr.ee/yash_gokakkar"
+            target="_blank"
+            className="btn"
+          >
             <FaRegHandshake size={30} />
-            
-            </a>
-            
+          </a>
+        </div>
+        <div className="portfolio_img">
+          <img src={IMG2} alt="" />
+          <h3> ASHUTOSH RAJ GUPTA </h3>
+          <a href={cv2} target="_blank" download className="btn">
+            <FiDownload size={30} />
+          </a>
+          <a href="https://linktr.ee/ashu_2643" target="_blank" className="btn">
+            <FaRegHandshake size={30} />
+          </a>
         </div>
       </div>
     </section>
