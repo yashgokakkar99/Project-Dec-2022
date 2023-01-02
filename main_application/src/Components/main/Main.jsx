e2e8161ef1bdb489fd28a7cf0425b7a032dad385
Yash Gokakkar <yashgokakkar@gmail.com>
2023-01-02 21:29:29 +0530

    swipper addedin main

--- main_application/src/Components/main/Main.jsx
+++ main_application/src/Components/main/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Main.css";
 import sampleimg from "../../assets/suggestion.png";
 import sampleimg1 from "../../assets/Images/android.png";
@@ -14,116 +14,147 @@ import cfron3 from "../../assets/courses/cfron3.png";
 import cfron4 from "../../assets/courses/cfron4.png";
 import Lottie from "lottie-react";
 import cs1 from "../../assets/lotties/coming_soon.json";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
 
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Main = () => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
   return (
     <section id="Main">
-      <div className="maindiv_content" id='yash_div'>
+      <div className="maindiv_content" id="yash_div">
         <div className="offi_websites">
           <h4>Official websites</h4>
-          { <div className="marquee_div">
-            <marquee
-              id="rightContainer_ContentTable2_panel3"
-              align="justify"
-              direction="up"
-              onmouseout="this.start()"
-              height="200px"
-              onmouseover="this.stop()"
-              scrollamount="3"
-              scrolldelay="60"
-            >
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://html.com/">
-                    HTML
-                  </a>
-                </p>
-              </div>
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="blank">
-                    HTML | MDN
-                  </a>
-                </p>
-              </div>
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://www.w3schools.com/html/8" target="blank">
-                    HTML | w3schools
-                  </a>
-                </p>
-              </div>
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="blank">
-                    CSS | MDN
-                  </a>
-                </p>
-              </div>
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://www.w3schools.com/css/" target="blank">
-                    CSS | w3schools
-                  </a>
-                </p>
-              </div>
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://www.geeksforgeeks.org/css/" target="blank">
-                    CSS | geeks for geeks
-                  </a>
-                </p>
-              </div>
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScrip" target="blank">
-                    Javascript | MDN
-                  </a>
-                </p>
-              </div>
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://www.w3schools.com/js/" target="blank">
-                    Javascript | w3schools
-                  </a>
-                </p>
-              </div>
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://www.geeksforgeeks.org/javascript/" target="blank">
-                    Javascript | geeks for geeks
-                  </a>
-                </p>
-              </div>
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://reactjs.org/tutorial/tutorial.html" target="blank">
-                    ReactJs
-                  </a>
-                </p>
-              </div>
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://www.w3schools.com/REACT/DEFAULT.ASP" target="blank">
-                    ReactJs | w3schools
-                  </a>
-                </p>
-              </div>
-              <div className="site1">
-                <p align="justify">
-                  <a href="https://www.geeksforgeeks.org/reactjs-tutorials/" target="blank">
-                    ReactJs | geeks for geeks
-                  </a>
-                </p>
-              </div>
-            </marquee>
-          </div> }
+          {
+            <div className="marquee_div">
+              <marquee
+                id="rightContainer_ContentTable2_panel3"
+                align="justify"
+                direction="up"
+                onmouseout="this.start()"
+                height="200px"
+                onmouseover="this.stop()"
+                scrollamount="3"
+                scrolldelay="60"
+              >
+                <div className="site1">
+                  <p align="justify">
+                    <a href="https://html.com/">HTML</a>
+                  </p>
+                </div>
+                <div className="site1">
+                  <p align="justify">
+                    <a
+                      href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+                      target="blank"
+                    >
+                      HTML | MDN
+                    </a>
+                  </p>
+                </div>
+                <div className="site1">
+                  <p align="justify">
+                    <a href="https://www.w3schools.com/html/8" target="blank">
+                      HTML | w3schools
+                    </a>
+                  </p>
+                </div>
+                <div className="site1">
+                  <p align="justify">
+                    <a
+                      href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+                      target="blank"
+                    >
+                      CSS | MDN
+                    </a>
+                  </p>
+                </div>
+                <div className="site1">
+                  <p align="justify">
+                    <a href="https://www.w3schools.com/css/" target="blank">
+                      CSS | w3schools
+                    </a>
+                  </p>
+                </div>
+                <div className="site1">
+                  <p align="justify">
+                    <a href="https://www.geeksforgeeks.org/css/" target="blank">
+                      CSS | geeks for geeks
+                    </a>
+                  </p>
+                </div>
+                <div className="site1">
+                  <p align="justify">
+                    <a
+                      href="https://developer.mozilla.org/en-US/docs/Web/JavaScrip"
+                      target="blank"
+                    >
+                      Javascript | MDN
+                    </a>
+                  </p>
+                </div>
+                <div className="site1">
+                  <p align="justify">
+                    <a href="https://www.w3schools.com/js/" target="blank">
+                      Javascript | w3schools
+                    </a>
+                  </p>
+                </div>
+                <div className="site1">
+                  <p align="justify">
+                    <a
+                      href="https://www.geeksforgeeks.org/javascript/"
+                      target="blank"
+                    >
+                      Javascript | geeks for geeks
+                    </a>
+                  </p>
+                </div>
+                <div className="site1">
+                  <p align="justify">
+                    <a
+                      href="https://reactjs.org/tutorial/tutorial.html"
+                      target="blank"
+                    >
+                      ReactJs
+                    </a>
+                  </p>
+                </div>
+                <div className="site1">
+                  <p align="justify">
+                    <a
+                      href="https://www.w3schools.com/REACT/DEFAULT.ASP"
+                      target="blank"
+                    >
+                      ReactJs | w3schools
+                    </a>
+                  </p>
+                </div>
+                <div className="site1">
+                  <p align="justify">
+                    <a
+                      href="https://www.geeksforgeeks.org/reactjs-tutorials/"
+                      target="blank"
+                    >
+                      ReactJs | geeks for geeks
+                    </a>
+                  </p>
+                </div>
+              </marquee>
+            </div>
+          }
         </div>
         <div className="yt_videos">
           <h4>Youtube videos</h4>
           <div className="img_marquee">
-            <marquee
+            {/* <marquee
               id="rightContainer_ContentTable2_panel3"
               align="justify"
               direction="up"
@@ -173,13 +204,78 @@ const Main = () => {
                   </a>
                 </p>
               </div>
-            </marquee>
+            </marquee> */}
+            <Swiper
+              spaceBetween={30}
+              centeredSlides={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Autoplay, Pagination, Navigation]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                {" "}
+                <a
+                  href="https://www.youtube.com/@TraversyMedia/playlists"
+                  target="blank"
+                >
+                  <img className="im1" src={fron1} />
+                </a>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p align="justify">
+                  <a
+                    href="https://www.youtube.com/@funfunfunction/playlists"
+                    target="blank"
+                  >
+                    <img className="im1" src={fron2} />
+                  </a>
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p align="justify">
+                  <a
+                    href="https://youtube.com/watch?v=pmKyG3NBY_k"
+                    target="blank"
+                  >
+                    <img className="im1" src={fron3} />
+                  </a>
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <p align="justify">
+                  <a
+                    href="https://www.youtube.com/watch?v=PkZNo7MFNFg&list=PLWKjhJtqVAbleDe3_ZA8h3AO2rXar-q2V"
+                    target="blank"
+                  >
+                    <img className="im1" src={fron4} />
+                  </a>
+                </p>
+              </SwiperSlide>
+              <SwiperSlide>
+                <p align="justify">
+                  <a
+                    href="https://www.youtube.com/@NetNinja/playlists"
+                    target="blank"
+                  >
+                    <img className="im1" src={fron5} />
+                  </a>
+                </p>
+              </SwiperSlide>
+            </Swiper>
           </div>
         </div>
         <div className="Projects">
           <h4>Projects</h4>
           <div className="project_con">
-          <Lottie animationData={cs1}/>
+            <Lottie animationData={cs1} />
             {/* <div className="level3">
               <h4>Advanced</h4>
             </div>
@@ -198,7 +294,9 @@ const Main = () => {
               <img src={road_im}></img>
             </div>
             <div className="button">
-              <a href="https://roadmap.sh/frontend" target="_blank">Get the road map</a>
+              <a href="https://roadmap.sh/frontend" target="_blank">
+                Get the road map
+              </a>
             </div>
           </div>
         </div>
@@ -206,7 +304,7 @@ const Main = () => {
         <div className="Courses">
           <h4>Courses</h4>
           <div className="course_marquee">
-            <marquee
+            {/* <marquee
               id="rightContainer_ContentTable2_panel3"
               align="justify"
               direction="down"
@@ -239,7 +337,52 @@ const Main = () => {
                   <img className="im1" src={cfron4} />
                 </a>
               </div>
-            </marquee>
+            </marquee> */}
+            <Swiper
+              spaceBetween={30}
+              centeredSlides={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Autoplay, Pagination, Navigation]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                {" "}
+                <a
+                  href="https://egghead.io/courses/the-beginner-s-guide-to-react"
+                  target="blank"
+                >
+                  <img className="im1" src={cfron1} />
+                </a>
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <a href="https://fireship.io/courses/js/" target="blank">
+                  <img className="im1" src={cfron2} />
+                </a>
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <a
+                  href="https://www.udemy.com/course/the-complete-web-development-bootcamp/"
+                  target="blank"
+                >
+                  <img className="im1" src={cfron3} />
+                </a>
+              </SwiperSlide>
+              <SwiperSlide>
+                {" "}
+                <a href="https://frontendmasters.com/" target="blank">
+                  <img className="im1" src={cfron4} />
+                </a>
+              </SwiperSlide>
+            </Swiper>
           </div>
         </div>
 
@@ -259,7 +402,7 @@ const Main = () => {
             ></marquee>
             <div className="texts">
               {/* <p>Suggestion</p> */}
-              <a href='#Suggestion'> Suggestion</a>
+              <a href="#Suggestion"> Suggestion</a>
             </div>
           </div>
         </div>
